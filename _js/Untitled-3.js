b456845e00e1cb1a7d118393801e0fe5363e8fb7
unknown <sanazganji66@gmail.com>
2017-09-11 18:39:34 +0430

    add and comimit

---
+++ _js/Untitled-3.js
@@ -0,0 +1,15 @@
+// JavaScript Document
+
+$(document).ready(function(e) {
+    $("#btn1").click(function(){
+		alert("text : "+ $("#test").text());
+		
+	});
+	
+	$("#btn2").click(function(){
+		alert("html : " + $("#test").html());
+		
+	});
+	
+
+});
